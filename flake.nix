9c63e63b7bac5b15b2b5955af88fb3e12b0d3610
Aamir Syed <aamir.syed@saronic.com>
2026-05-04 15:07:12 -0500

    chore: remove det-nix and magic-nix-cache from nix dev shell inputs

--- flake.nix
+++ flake.nix
@@ -2,9 +2,7 @@
   description = "FlakeHub Cache Buildkite Plugin";
 
   inputs = {
-    determinate.url = "https://flakehub.com/f/DeterminateSystems/determinate/3";
     flake-utils.url = "github:numtide/flake-utils";
-    magic-nix-cache.url = "https://flakehub.com/f/DeterminateSystems/magic-nix-cache/0.1.547";
     nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.2511";
   };
 
